b6e8cd3c3a8a3af629880314a840f864b7035a93
Aluno <Aluno@LAB04-PC09>
2024-06-12 20:45:53 -0300

    Commit project Parsing01

---
+++ src/Parsing01.java
@@ -0,0 +1,26 @@
+public class Parsing01 {
+    public static void main(String[] args) {
+        //Declare and intitialize 3 Strings: shirtPrice, taxRate, and gibberish
+        
+        String shirtPrice = "15";
+        int intVarl = Integer.parseInt(shirtPrice);
+        System.out.println(intVarl);
+        
+        String textRate = "0.05";
+        double doubleVar2 = Double.parseDouble(textRate);
+        System.out.println(doubleVar2);
+        
+        String gibberish = "88ds7nds87dsfs";
+        System.out.println(gibberish);
+        
+        //Parse shirtPrice and taxRate, and print the total tax
+        
+        
+        
+        //Try to parse taxRate as an int
+        
+        //Try to parse gibberish as an int
+        
+    }
+    
+}
